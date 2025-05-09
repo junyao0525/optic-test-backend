9e45289d574d230d7b7665517b94cf951e533067
junyao <junyao0375@gmail.com>
2025-05-09 11:23:09 +0800

    fix : error requirements

--- routers/mediaPipe.py
+++ routers/mediaPipe.py
@@ -23,7 +23,7 @@ mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
 face_mesh = mp_face_mesh.FaceMesh(
     static_image_mode=True,
-    max_num_faces=5,
+    max_num_faces=1,
     refine_landmarks=True,
     min_detection_confidence=0.5
 )
